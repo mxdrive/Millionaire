f80e4c069d168328af24dd6c079c9d898c570fcc
mxdrive <mxdrive@rambler.ru>
2018-08-14 19:38:54 +0300

    add user interface class

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
+import javafx.application.Application;
+
 public class Main {
 	// For the first group of five questions: £100 -> £200 -> £300 -> £500 -> £1,000
 	// For the second group of five questions: £2,000 -> £4,000 -> £8,000 -> £16,000 -> £32,000
 	// For the final group of five questions: £64,000 -> £125,000 -> £250,000 -> £500,000 -> £1,000,000
-	public static void main(String args []) {
-		new Millionaire().millionaire();
+//	public static void main(String args []) {
+//		new Millionaire().millionaire();
+//	}
+
+	public static void main(String args[]){
+		Application.launch(UserInterface.class, args);
 	}
 }
---
+++ src/UserInterface.java
@@ -0,0 +1,66 @@
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserInterface extends Application {
+    private List<Button> buttonsList = new ArrayList<>();
+
+    @Override
+    public void start(Stage primaryStage) {
+        buttonsList.add(createButton("answer 1", 100, 200));
+        buttonsList.add(createButton("answer 2", 200, 200));
+        buttonsList.add(createButton("answer 3", 300, 200));
+        buttonsList.add(createButton("answer 4", 400, 200));
+
+        //TODO wrap with if-else if hints
+        buttonsList.add(createButton("hint", 150, 200));
+
+        buttonsList.get(0).setOnMouseClicked((new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent event) {
+                System.out.println("Hello World");
+            }
+        }));
+
+        buttonsList.get(1).setOnMouseClicked((new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent event) {
+                System.out.println("Hello World");
+            }
+        }));
+        //Creating a Group object
+        Group root = new Group();
+
+        for (Button button : buttonsList) {
+            root.getChildren().add(button);
+        }
+
+        System.out.println(root.getChildren());
+
+        //Creating a scene object
+        Scene scene = new Scene(root, 600, 300);
+        scene.setFill(Color.LAVENDER);
+
+        //Setting title to the Stage
+        primaryStage.setTitle("Convenience Methods Example");
+
+        //Adding scene to the stage
+        primaryStage.setScene(scene);
+
+        //Displaying the contents of the stage
+        primaryStage.show();
+    }
+
+    private Button createButton(String buttonText, int layoutX, int layoutY) {
+        Button button = new Button(buttonText);
+        button.setLayoutX(layoutX);
+        button.setLayoutY(layoutY);
+        return button;
+    }
+}
